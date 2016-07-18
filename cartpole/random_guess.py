c24ff02dcf15058ec26b208cd00cab277bd3a78d
Jonathan Gray <jonathan@pluslabs.org>
2016-07-17 18:02:22 -0700

    modify random_guess.py to match blog code

--- cartpole/random_guess.py
+++ cartpole/random_guess.py
@@ -1,59 +1,38 @@
-'''
-OpenAI Request for Research: Cartpole
-
-Implements a simple linear model. Generates 10k random models and evaluates
-each on a small number of episodes. Chooses the model that performs best and
-tests it on 100 episodes.
-
-Notes:
-  - model params are chosen from a uniform random distribution [-1, 1)
-  - the first model to last 200 steps on 20 consecutive episodes (a perfect
-    validation score) is chosen as the winner, so in practice fewer than 10k
-    models are generated.
-
-'''
-
 import gym
 import numpy as np
 
-MAX_MODELS   = 10000  # models to try
-MAX_EPISODES = 20     # episodes per model
-MAX_STEPS    = 200    # steps per episode
-OUTPUT_DIR   = '/tmp/cartpole-random-guess'
+MAX_EPISODES = 20  # episodes per model
+MAX_STEPS = 200  # max steps per episode
+OUTPUT_DIR = '/tmp/cartpole-random-guess'
 
-def run_episode(env, model):
-  ''' play through an episode and return the reward '''
-  ob = env.reset()
+def get_action(w, s):
+  return int(np.dot(w, s) > 0)
+
+def run_episode(env, w):
+  ''' returns the total reward over the episode '''
+  s = env.reset()
   done = False
   reward = 0.0
-  for s in xrange(MAX_STEPS):
-    action = int(np.dot(model, ob) > 0)
-    ob, r, done, _ = env.step(action)
+  for _ in xrange(MAX_STEPS):
+    a = get_action(w, s)
+    s, r, done, _ = env.step(a)
     reward += r
     if done:
       break
   return reward
 
-
 if __name__ == '__main__':
   env = gym.make('CartPole-v0')
-  best_r, best_m = None, None
-  env.monitor.start(OUTPUT_DIR, force=True)
+  env.monitor.start(OUTPUT_DIR)
 
-  # try differrent models
-  for i in xrange(MAX_MODELS):
-    m = np.random.rand(4) * 2 - 1  # uniform random [-1, 1)
-    reward = sum(run_episode(env, m) for _ in xrange(MAX_EPISODES))
-    print i, reward, m
-    if reward > best_r:
-      best_r, best_m = reward, m
+  while True:
+    w = np.random.rand(4) * 2 - 1  # uniform random [-1, 1)
+    reward = sum(run_episode(env, w) for _ in xrange(MAX_EPISODES))
     if reward == MAX_EPISODES * MAX_STEPS:
-      # exit early on a perfect score, we will end up choosing this model
       break
 
-  # evaluate the best model over 100 episodes
-  print 'BEST', best_m
+  # evaluate the best model for 100 episodes
   for _ in xrange(100):
-    run_episode(env, best_m)
+    run_episode(env, w)
 
   env.monitor.close()
