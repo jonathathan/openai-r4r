4b72f212546ef8d98a662dad05acbdbe7ba5204f
Jonathan Gray <jonathan@pluslabs.org>
2016-07-18 19:58:55 -0700

    random_guess.py: formatting

--- cartpole/random_guess.py
+++ cartpole/random_guess.py
@@ -2,8 +2,8 @@ import gym
 import numpy as np
 
 MAX_EPISODES = 20  # episodes per model
-MAX_STEPS = 200    # max steps per episode
-OUTPUT_DIR = '/tmp/cartpole-random-guess'  # env monitor output goes here
+MAX_STEPS    = 200 # max steps per episode
+OUTPUT_DIR   = '/tmp/cartpole-random-guess'  # env monitor output goes here
 
 def get_action(w, s):
   return int(np.dot(w, s) > 0)
@@ -23,16 +23,8 @@ def run_episode(env, w):
 
 if __name__ == '__main__':
   env = gym.make('CartPole-v0')
-  env.monitor.start(OUTPUT_DIR)
 
-  while True:
+  for i in xrange(10000):
     w = np.random.rand(4) * 2 - 1  # uniform random [-1, 1)
     reward = sum(run_episode(env, w) for _ in xrange(MAX_EPISODES))
-    if reward == MAX_EPISODES * MAX_STEPS:
-      break
-
-  # evaluate the best model for 100 episodes
-  for _ in xrange(100):
-    run_episode(env, w)
-
-  env.monitor.close()
+    print reward / MAX_EPISODES
